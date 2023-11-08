d6f1a7853d55a95cfff0b3ebf5a505b5a6468ba6
leemingoo <dwmapi@naver.com>
2023-11-08 10:34:58 +0900

    feat: 비교 함수 생성

--- module/compare-data.js
+++ module/compare-data.js
@@ -1,3 +1,11 @@
-export function compareData(inputData, outputData) {
-
+/**
+ * 비교 결과 반환
+ * @param {*} inputJSONData - 첫 번째 JSON 데이터.
+ * @param {*} outputJSONData - 두 번째 JSON 데이터.
+ * @returns {boolean} - 데이터가 동일한지 여부를 반환.
+ */
+export function compareData(inputJSONData, outputJSONData) {
+  const inputStringified = JSON.stringify(inputJSONData);
+  const outputStringified = JSON.stringify(outputJSONData);
+  return inputStringified === outputStringified;
 }
