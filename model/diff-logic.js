4297728339355965bc93c861645ccaed7de5d880
leemingoo <dwmapi@naver.com>
2023-11-08 14:30:24 +0900

    프로젝트 시작을 위한 임시 저장

--- model/diff-logic.js
+++ model/diff-logic.js
@@ -1,4 +1,4 @@
-import { writeJSONFile } from "./write-json.js";
+import { readFileSync } from 'fs';
 /**
  * 
  * @param {JSON, Path} inputJSONdata 
@@ -10,11 +10,28 @@ export default function(id, pw, userDBPath, adminDBPath) {
   if (!userDBPath.endsWith('.json') || !adminDBPath.endsWith('.json')) {
     throw new Error(`매개변수 ${userDBPath}, ${adminDBPath}는 json 파일이 아닙니다.`);
   }
-  let result = {};
+
+  const readUserData = (filePath) => {
+    const data = readFileSync(filePath, 'utf8');
+    return JSON.parse(data);
+  };
+
+  const adminData = readUserData(adminDBPath);
+  const userData = readUserData(userDBPath);
   
+  const isAdmin = adminData.id === id && adminData.pw === pw;
+  const isUser = userData.id.includes(id) && userData.pw[userData.id.indexOf(id)] === pw;
+
+  if (isAdmin) {
+    return 'admin'; // 관리자로 로그인
+  } else if (isUser) {
+    return 'user'; // 유저로 로그인
+  } else {
+    return 'unauthorized'; // 인증되지 않음
+  }
 
   // const inputStringified = JSON.stringify(inputJSONData);
   // const outputStringified = JSON.stringify(outputJSONData);
 
-  return result;
+  //return result;
 }
--- model/write-json.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-export function writeJSONFile(filePath, data) {
-  try {
-    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-  } catch (error) {
-    throw new Error(`결과를 파일에 쓸 수 없습니다: ${filePath}`);
-  }
-}
